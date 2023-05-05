deb2787594b6dc337261a2459a441071752d1d2f
Alexander <91263783+Twilight-Al-Sparkle@users.noreply.github.com>
2023-05-05 20:02:47 +0300

    Configure main.ts for vercel

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import ejs from 'ejs';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'views'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.set('views', join(__dirname, 'views'));
+  app.use('/static', express.static('src/views'));
   app.setViewEngine('ejs');
+  app.engine('ejs', ejs.__express);
 
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
